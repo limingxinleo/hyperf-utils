64c20ec1fe297ac7de2d52530cd9274663cb290c
李铭昕 <715557344@qq.com>
2020-07-14 19:49:13 +0800

    Create Service.php

---
+++ src/Service.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfX\Utils;
+
+use Hyperf\Contract\StdoutLoggerInterface;
+use Psr\Container\ContainerInterface;
+
+abstract class Service
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * @var Factory
+     */
+    protected $factory;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+        $this->logger = $container->get(StdoutLoggerInterface::class);
+        $this->factory = $container->get(Factory::class);
+    }
+}
