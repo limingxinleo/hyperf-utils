04edabf65a630a9a4438a95b7c3d22e629481d00
李铭昕 <715557344@qq.com>
2020-07-21 12:00:32 +0800

    Update date->load().

--- src/Utils/Date.php
+++ src/Utils/Date.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
@@ -20,7 +20,7 @@ class Date
      */
     public function load($date): ?Carbon
     {
-        if (is_int($date)) {
+        if (is_numeric($date)) {
             return Carbon::createFromTimestamp($date);
         }
 
--- tests/Cases/DateTest.php
+++ tests/Cases/DateTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
  * This file is part of Hyperf.
  *
  * @link     https://www.hyperf.io
- * @document https://doc.hyperf.io
+ * @document https://hyperf.wiki
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
@@ -33,5 +33,11 @@ class DateTest extends AbstractTestCase
 
         $carbon = $util->load(null);
         $this->assertNull($carbon);
+
+        $carbon = $util->load(1577836800);
+        $this->assertSame(1577836800, $carbon->getTimestamp());
+
+        $carbon = $util->load('1577836800');
+        $this->assertSame(1577836800, $carbon->getTimestamp());
     }
 }
