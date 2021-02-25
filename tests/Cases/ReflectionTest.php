ac2fd9da3ac12dbfd7584caf8239df0aa83fb07e
李铭昕 <715557344@qq.com>
2021-02-25 17:31:26 +0800

    Added function `invoke`

---
+++ tests/Cases/ReflectionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace HyperfTest\Cases;
+
+use HyperfTest\Stub\Model;
+use function HyperfX\Utils\Reflection\invoke;
+
+/**
+ * @internal
+ * @coversNothing
+ */
+class ReflectionTest extends AbstractTestCase
+{
+    public function testInvoke()
+    {
+        $fillable = invoke(new Model())->fillable;
+
+        $this->assertSame(['id', 'type', 'message', 'keyword'], $fillable);
+    }
+}
