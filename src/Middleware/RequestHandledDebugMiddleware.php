33ccdda1a9a0c9e01ffe93124534f6e43bfa9ffe
李铭昕 <715557344@qq.com>
2021-06-03 11:28:28 +0800

    Create RequestHandledDebugMiddleware.php

---
+++ src/Middleware/RequestHandledDebugMiddleware.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://hyperf.wiki
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+namespace Han\Utils\Middleware;
+
+use Hyperf\Logger\LoggerFactory;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class RequestHandledDebugMiddleware implements MiddlewareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $time = microtime(true);
+        try {
+            $response = $handler->handle($request);
+        } catch (\Throwable $exception) {
+            throw $exception;
+        } finally {
+            $logger = $this->container->get(LoggerFactory::class)->get('request');
+
+            // 日志
+            $time = microtime(true) - $time;
+            $debug = 'URI: ' . (string) $request->getUri() . PHP_EOL;
+            $debug .= 'TIME: ' . $time . PHP_EOL;
+            $debug .= 'REQUEST: ' . $this->getRequestString($request) . PHP_EOL;
+            if (isset($response)) {
+                $debug .= 'RESPONSE: ' . $this->getResponseString($response) . PHP_EOL;
+            }
+            if (isset($exception) && $exception instanceof \Throwable) {
+                $debug .= 'EXCEPTION: ' . $exception->getMessage() . PHP_EOL;
+            }
+
+            if ($time > 1) {
+                $logger->error($debug);
+            } else {
+                $logger->info($debug);
+            }
+        }
+
+        return $response;
+    }
+
+    protected function getResponseString(ResponseInterface $response): string
+    {
+        return (string) $response->getBody();
+    }
+
+    protected function getRequestString(ServerRequestInterface $request): string
+    {
+        $result = '';
+        foreach ($request->getHeaders() as $header => $values) {
+            foreach ((array) $values as $value) {
+                $result .= $header . ': ' . $value . PHP_EOL;
+            }
+        }
+
+        $result .= (string) $request->getBody();
+        return $result;
+    }
+
+    protected function getCustomData(): string
+    {
+        return '';
+    }
+}
