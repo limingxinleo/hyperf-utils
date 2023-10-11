81eb11e9cdf2087dbe59f778aae25542efa34fd3
李铭昕 <715557344@qq.com>
2023-10-11 14:44:38 +0800

    Format code

--- src/Caster/IntegerItems.php
+++ src/Caster/IntegerItems.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
  * @contact  group@hyperf.io
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
+
 namespace Han\Utils\Caster;
 
 class IntegerItems implements \Countable
